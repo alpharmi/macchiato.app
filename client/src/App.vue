514a71f859618d464ad26931da51641b5918254c
alpharmi <gianni.s177@outlook.com>
2024-12-20 16:24:54 +1100

    mobile optimisations

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <Analytics/>
 
-  <header class="h-14 bg-[#00000020] backdrop-blur-lg drop-shadow-md flex items-center pl-10 gap-1">
+  <header class="h-14 bg-[#00000020] backdrop-blur-lg drop-shadow-md flex items-center sm:pl-2 pl-10 gap-1">
     <img @click="redirect('/')" class="logo h-3/5" src="/logo.webp" alt="logo">
     <p class="text-2xl"><span class="text-description">{{ currentRoute() }}</span></p>
   </header>
 
-  <main class="w-full h-max p-12 sm:p-0 min-h-[calc(100vh-3.5rem)]">
+  <main class="w-full h-max p-12 sm:p-3 !pt-12 min-h-[calc(100vh-3.5rem)]">
     <RouterView/>
   </main>
 
-  <footer class="h-20 w-full bg-[#0a0a0a] flex flex-col items-center justify-center text-description">
+  <footer class="h-auto min-h-20 w-full p-3 text-center bg-[#0a0a0a] flex flex-col items-center justify-center text-description">
     <p>macchiato</p>
     <p>Girls' Frontline 2: Exilium and content are trademarks and copyright of SUNBORN Information or MicaTeam.</p>
   </footer>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,14 +2,14 @@
     <div class="relative flex h-auto flex-col mb-20">
         <div class="w-full h-full flex flex-wrap justify-center gap-5 mb-32 sm:-mb-10">
             <div class="flex flex-col gap-5">
-                <div class="main-container w-[28rem] h-[9rem] flex flex-col p-2 relative">
+                <div class="main-container max-w-[28rem] min-h-[9rem] flex flex-col p-2 relative">
                     <p class="font-bold bg-secondary w-max px-3 -translate-x-3 text-xl">Welcome</p>
                     <p><span class="text-highlight z-10">macchiato</span> currently includes a Gacha Counter, and general game information for Girls' Frontline 2. Please note the site is still in development and some features may not work as expected.</p>
                     <p class="absolute font-black right-0 bottom-0 text-7xl opacity-[0.02]">WELCOME</p>
                 </div>
             </div>
             <div class="flex flex-col gap-5">
-                <div class="main-container w-[28rem] h-[9rem] flex flex-col p-2 relative pt-8 gap-1">
+                <div class="main-container max-w-[28rem] min-h-[9rem] flex flex-col p-2 relative pt-8 gap-1">
                     <p>Import your Pull History to view your latest and previous pulls for Characters and Weapons on all banners.</p>
                     <button @click="redirect('/gacha')" class="button w-max inline-image"><img src="/items/targeted_access_permission.webp" alt="item"> Gacha Tracker</button>
                     <p class="absolute font-black right-0 bottom-0 text-7xl opacity-[0.02]">GACHA</p>
