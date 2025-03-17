e1a0ebbfb9e8be0ff3b530a680d8f7c5b5363d8e
alpharmi <gianni.s177@outlook.com>
2025-03-17 17:09:34 +1100

    Update PrivacyPolicy.vue

--- client/src/views/PrivacyPolicy.vue
+++ client/src/views/PrivacyPolicy.vue
@@ -4,8 +4,6 @@
             <p class="text-xl pb-2">What <span class="text-highlight">macchiato.app</span> collects</p>
             <p class="font-bold">• Visited macchiato.app</p>
             <p>macchiato.app uses privacy-friendly <a href="https://plausible.io/login">plausible.io</a> analytics <span class="text-description">(referer, browser, operating system, device type, visitor country)</span> to see how many people use specific features and what to priortise or implement.</p>
-            <p class="font-bold pt-2">• Pull History</p>
-            <p>macchiato.app does save your pull data locally to your own browser to avoid having to constantly import. You can clear your data by clearing your browser's LocalStorage.</p>
             <p class="text-xl pt-5">What <span class="text-highlight">macchiato.app</span> DOESN'T collect</p>
             <p>macchiato.app will never save user data, such as passwords, UIDs, usernames, emails, etc.</p>
             <p class="text-xl pt-5">Open source</p>
